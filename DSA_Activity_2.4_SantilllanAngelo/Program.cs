7ef93be0b0e46b604457fbf1ac8fa1151b48a65c
lkjeloingfd <hed-avsantillan@smu.edu.ph>
2025-11-06 08:26:52 +0800

    added method

--- DSA_Activity_2.4_SantilllanAngelo/Program.cs
+++ DSA_Activity_2.4_SantilllanAngelo/Program.cs
@@ -10,6 +10,43 @@ namespace DSA_Activity_2._4_SantilllanAngelo
     {
         static void Main(string[] args)
         {
+            Queue<string> jobs = new Queue<string>();
+
+            Console.WriteLine("Welcome to my JOB SCHEDULING");
+            int choice = 0;
+            do 
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter Job Name: ");
+                        string newJob = Console.ReadLine();
+                        jobs.Enqueue(newJob);
+                        Console.WriteLine($"Job '{newJob}' added to the queue.");
+                        break;
+
+
+                }
+
+
+                if (choice != 7)
+                {
+                    DisplayMenu();
+                }
+            }while (choice != 7);
+
+        }
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Choose what you want to do?");
+            Console.WriteLine("1. New Job");
+            Console.WriteLine("2. Finish Job");
+            Console.WriteLine("3. Search Job");
+            Console.WriteLine("4. Current Job");
+            Console.WriteLine("5. Display All Jobs");
+            Console.WriteLine("6. Count Job");
+            Console.WriteLine("7. Exit");
+            Console.Write("Enter your choice: ");
         }
     }
 }
